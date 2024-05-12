f839f239c85c7a45b5d4df0c8f014a63783c3d12
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2024-05-12 18:31:10 -0400

    starter UI

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+	import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import { Avatar } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+	import { initializeStores, Drawer } from '@skeletonlabs/skeleton';
+	let drawerState = 'closed';
+	initializeStores();
+	import { getDrawerStore } from '@skeletonlabs/skeleton';
+	const drawerStore = getDrawerStore();
+	$: drawerposition = 'bottom' as 'bottom' | 'right';
+	onMount(() => {
+		if (window.matchMedia('(min-width: 1024px)').matches) {
+			drawerposition = 'right';
+		}
+		window.addEventListener('resize', () => {
+			if (window.matchMedia('(min-width: 1024px)').matches) {
+				drawerposition = 'right';
+			} else {
+				drawerposition = 'bottom';
+			}
+		});
+	});
+</script>
+
+{#key drawerposition}
+	<Drawer position={drawerposition} width={drawerposition === 'right' ? 'w-96' : ''}>
+		{#if $drawerStore.id === 'drawer1'}
+			<h1>this is example 1</h1>
+		{:else if $drawerStore.id === 'drawer2'}
+			<h1>this is example 2</h1>
+		{:else}
+			<h1>default</h1>
+		{/if}
+	</Drawer>
+{/key}
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	<svelte:fragment slot="lead">(icon)</svelte:fragment>
+	<h1>hello there</h1>
+	<svelte:fragment slot="trail"
+		><Avatar initials="JD" background="bg-primary-500" /></svelte:fragment
+	>
+</AppBar>
+<main></main>
+<TabGroup
+	justify="justify-center"
+	active="variant-filled-primary"
+	hover="hover:variant-soft-primary"
+	flex="flex-1 lg:flex-none"
+	rounded=""
+	border=""
+	regionList="lg:flex lg:flex-col"
+	class="bg-surface-100-800-token fixed bottom-0 z-50 w-full lg:bottom-0 lg:right-0 lg:top-0 lg:z-50 lg:h-full lg:w-20"
+>
+	<TabAnchor
+		on:click={() => {
+			if (drawerState === 'drawer1') {
+				drawerStore.close();
+				drawerState = 'closed';
+			} else {
+				drawerStore.open({ id: 'drawer1' });
+				drawerState = 'drawer1';
+			}
+		}}
+		selected={drawerState === 'drawer1'}
+	>
+		<svelte:fragment slot="lead">(icon1)</svelte:fragment>
+		<span>index</span>
+	</TabAnchor>
+	<TabAnchor
+		on:click={() => {
+			if (drawerState === 'drawer2') {
+				drawerStore.close();
+				drawerState = 'closed';
+			} else {
+				drawerStore.open({ id: 'drawer2' });
+				drawerState = 'drawer2';
+			}
+		}}
+		selected={drawerState === 'drawer2'}
+	>
+		<svelte:fragment slot="lead">(icon2)</svelte:fragment>
+		<span>home</span>
+	</TabAnchor>
+</TabGroup>
