4f98df90a763acc2c4a06690feffbe06fd1dd954
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2024-05-16 14:24:27 -0400

    Add CourseSection component
    ```

--- src/components/Calender.svelte
+++ src/components/Calender.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import CourseSection from './CourseSection.svelte';
+
 	// Initialize start and end dates
 	let startDate = new Date(2000, 0, 1, 7, 0, 0); // 7:00am
 	let endDate = new Date(2000, 0, 1, 22, 0, 0); // 10:00pm
@@ -19,9 +22,22 @@
 	generateDates();
 
 	// Generate an array of days of the week
-	let daysOfWeek = new Array(6).fill(0).map((_, i) => {
-		let date = new Date(2000, 0, i + 3);
+	let daysOfWeek = new Array(7).fill(0).map((_, i) => {
+		let date = new Date(2000, 0, i + 2);
 		return date.toLocaleDateString('en-us', { weekday: 'long' });
+	});
+	let dayWidth = 0;
+	let timeColWidth = 0;
+	let timeCol: HTMLDivElement;
+	let heightOfOneHour = 0;
+	onMount(() => {
+		function getHeightofOneHour() {
+			let numHours = (endDate.getTime() - startDate.getTime()) / 1000 / 60 / 60;
+			console.log(timeCol);
+			let height = timeCol?.clientHeight / numHours;
+			return height;
+		}
+		heightOfOneHour = getHeightofOneHour();
 	});
 </script>
 
@@ -29,17 +45,21 @@
 <div
 	class="align-center sticky top-0 z-30 flex w-[200vw] flex-row justify-evenly bg-surface-50 pt-5 lg:w-full"
 >
+	<p class="sticky left-0 z-30 flex-[.5] bg-surface-50 text-center" bind:clientWidth={timeColWidth}>
+		Time
+	</p>
 	{#each daysOfWeek as day}
-		<p class="text-center">{day}</p>
+		<p class="flex-1 text-center" bind:clientWidth={dayWidth}>{day}</p>
 	{/each}
 </div>
 
 <!-- Display the generated dates -->
-<div class="flex w-[200vw] flex-col lg:w-full">
+<div class="flex w-[200vw] flex-col lg:w-full" bind:this={timeCol}>
 	{#each dates as date, i}
 		{#if i % 2 === 0}
 			<p
-				class="text-cente sticky left-0 z-20 ml-1 mr-1 flex h-10 w-20 translate-y-1/2 transform items-center justify-center bg-surface-50"
+				class="sticky left-0 z-20 ml-1 mr-1 flex h-10 translate-y-1/2 transform items-center justify-center bg-surface-50 text-center"
+				style:width={timeColWidth + 'px'}
 			>
 				{date.toLocaleTimeString('en-us', { hour: 'numeric', minute: '2-digit' })}
 				<!-- format the date as a time like 6:00am -->
@@ -53,4 +73,29 @@
 				: 'opacity-25'}"
 		/>
 	{/each}
+	<CourseSection
+		top={heightOfOneHour * 8.5}
+		left={timeColWidth + dayWidth * 2}
+		width={dayWidth}
+		height={heightOfOneHour * 1.4}
+	>
+		<p class="text-center">Course 1</p>
+	</CourseSection>
+	<CourseSection
+		top={heightOfOneHour * 8.5}
+		left={timeColWidth + dayWidth * 4}
+		width={dayWidth}
+		height={heightOfOneHour * 1.4}
+	>
+		<p class="text-center">Course 1</p>
+	</CourseSection>
+	<CourseSection
+		top={heightOfOneHour * 12}
+		left={timeColWidth + dayWidth * 3}
+		width={dayWidth}
+		height={heightOfOneHour * 2.9}
+		color="!bg-blue-200"
+	>
+		<p class="text-center">Course 2</p>
+	</CourseSection>
 </div>
---
+++ src/components/CourseSection.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let top = 0;
+	export let left = 0;
+	export let width = 0;
+	export let height = 0;
+	export let color = '!bg-red-200';
+</script>
+
+<button
+	class="card card-hover absolute inline-block !rounded-md {color} py-2"
+	style="top: {top}px; left: {left}px; width: {width}px; height: {height}px;"
+>
+	<slot></slot>
+</button>
