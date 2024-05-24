cb6bbdd891654a9737dd5ab1cdea9c1709b2910a
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2024-05-24 10:07:06 -0400

    add derived store for humanReadableTerm

--- src/lib/termStore.ts
+++ src/lib/termStore.ts
@@ -1,3 +1,26 @@
-import { writable } from 'svelte/store';
+import { writable, derived } from 'svelte/store';
 export const termStore = writable(-1);
 // https://svelte.dev/docs/svelte-store
+export const humanReadableTerm = derived(termStore, ($term) => {
+	if ($term === -1) return 'Loading...';
+	let result = '';
+	switch ($term.toString().substring(4, 6)) {
+		case '90':
+			result = 'Fall ';
+			break;
+		case '10':
+			result = 'Spring ';
+			break;
+		case '50':
+			result = 'Summer ';
+			break;
+		case '95':
+			result = 'Winter ';
+			break;
+		default:
+			result = 'Unknown ';
+			break;
+	}
+	result += $term.toString().substring(0, 4);
+	return result;
+});
