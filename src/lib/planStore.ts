16a7fde8ca80e06a318d0d2052f834c021491442
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2024-05-24 10:59:24 -0400

    Update planStore to update termStore when a plan is selected

--- src/lib/planStore.ts
+++ src/lib/planStore.ts
@@ -1,5 +1,6 @@
 import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
+import { termStore } from './termStore';
 const defaultData = [] as Array<unknown>;
 const PLAN_KEY = 'SB_plans'; // key for local storage
 export const planStore = writable(
@@ -12,6 +13,16 @@ planStore.subscribe((value) => {
 		// if the browser is available, set the value in local storage
 		//if the old value is null, set the default value
 		localStorage.setItem(PLAN_KEY, JSON.stringify(value) ?? JSON.stringify(defaultData));
+	}
+
+	//when a plan is selected, update the term store
+	if (value.length > 0) {
+		//find the active plan
+		const activePlan = value.find((plan: { active: boolean }) => plan.active);
+		//if the active plan is found, update the term store
+		if (activePlan) {
+			termStore.set(activePlan.term);
+		}
 	}
 });
 // https://svelte.dev/docs/svelte-store
