25604a378d242c0839eb8b4d5c9118500e15a463
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2024-05-24 10:09:09 -0400

    Refactor themeStateStore

--- src/lib/themeStateStore.ts
+++ src/lib/themeStateStore.ts
@@ -1,9 +1,17 @@
 import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
-export const themeState = writable(browser ? (localStorage.getItem('theme') as string) : 'rocket');
+const defaultData = 'rocket'; // default theme
+const THEME_KEY = 'SB_theme'; // key for local storage
+export const themeState = writable(
+	browser && localStorage.getItem(THEME_KEY) !== null // check if the key exists in local storage
+		? (localStorage.getItem(THEME_KEY) as string) // if it does, parse the value
+		: defaultData // if it does, parse the value
+);
 themeState.subscribe((value) => {
 	if (browser) {
-		localStorage.setItem('theme', value ?? 'rocket');
+		// if the browser is available, set the value in local storage
+		//if the old value is null, set the default value
+		localStorage.setItem(THEME_KEY, value ?? defaultData);
 	}
 });
 // https://svelte.dev/docs/svelte-store
