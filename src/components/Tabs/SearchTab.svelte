498802d97436a64d4dbdfb1d359488afaefb5c9f
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2024-05-26 14:35:54 -0400

    Update searchCourses function to use new API endpoint and reduce api calls

--- src/components/Tabs/SearchTab.svelte
+++ src/components/Tabs/SearchTab.svelte
@@ -10,52 +10,27 @@
 
 	let search = '';
 	$: jsondata = [] as Array<any>;
+	let firstMinoftoday = new Date();
+	firstMinoftoday.setHours(0, 0, 0, 0);
 	async function searchCourses(searchval: string) {
 		let query = searchval.replace(/\s/g, ''); // remove spaces from the query
 		query = query.replace(/([a-zA-Z])([0-9])/gi, '$1 $2'); // add a space between the course letters and numbers
 		autocompleteOptions = await fetch(
-			'http://localhost:5173/api/courses?id=' + query + '&term=' + $termStore
+			'http://localhost:5173/api/courses2?course=' + query + '&term=' + $termStore
 		)
 			.then((res) => res.json())
 			.then((data) => {
 				console.log(data);
-
-				return data.courses.map((course: { _id: string; description: string }) => {
+				const coursearr = data.courses;
+				return coursearr.map((course: any) => {
 					return {
 						label: course._id,
 						value: course._id,
-						keywords: course.description.split(' ').join(', '),
-						meta: course
+						meta: course.sections
 					};
 				});
 			});
 	}
-	async function getSections(courseid: string) {
-		let query = courseid;
-		if ($termStore !== -1) {
-			// if the term is set, add it to the query
-			query += '&term=' + $termStore;
-		}
-		const response = await fetch('http://localhost:5173/api/sections?course=' + query);
-		let resdata = await response.json();
-		planStore.update((plans) => {
-			let activePlan = plans.find((p) => p.active);
-			//if course is already in the plan,remove it
-			if (activePlan) {
-				let courseIndex = activePlan.courses.findIndex((c) => c.course === courseid);
-				if (courseIndex !== -1) {
-					activePlan.courses = activePlan.courses.filter((c) => c.course !== courseid);
-				}
-				activePlan.courses.push({ course: courseid, sections: resdata.courses });
-			}
-
-			return plans;
-		});
-		search = '';
-		autocompleteOptions = [];
-	}
-	let firstMinoftoday = new Date();
-	firstMinoftoday.setHours(0, 0, 0, 0);
 	function selectSection(course: string, section: string) {
 		planStore.update((plans) => {
 			//mark the section as selected and unselect all other sections in that course
@@ -72,12 +47,22 @@
 			return plans;
 		});
 	}
-
-	async function onFlavorSelection(event: CustomEvent<AutocompleteOption<string>>): Promise<void> {
-		search = event.detail.label;
-		console.log(event.detail.meta);
-		let courseid: string = event.detail.meta as string;
-		getSections(courseid);
+	async function onSelection(event: CustomEvent<AutocompleteOption<string>>): Promise<void> {
+		const sections = event.detail.meta;
+		const course = event.detail.value;
+		planStore.update((plans) => {
+			let activePlan = plans.find((p) => p.active);
+			if (activePlan) {
+				let courseIndex = activePlan.courses.findIndex((c) => c.course === course);
+				if (courseIndex !== -1) {
+					activePlan.courses = activePlan.courses.filter((c) => c.course !== course);
+				}
+				activePlan.courses.push({ course: course, sections: sections });
+			}
+			return plans;
+		});
+		search = '';
+		autocompleteOptions = [];
 	}
 
 	let autocompleteOptions: AutocompleteOption<string>[] = [];
@@ -102,11 +87,7 @@
 </div>
 
 <div class="card max-h-48 w-full max-w-sm overflow-y-auto p-4" tabindex="-1">
-	<Autocomplete
-		bind:input={search}
-		options={autocompleteOptions}
-		on:selection={onFlavorSelection}
-	/>
+	<Autocomplete bind:input={search} options={autocompleteOptions} on:selection={onSelection} />
 </div>
 
 {#if jsondata?.courses?.length > 0}
