43efe6d170c3581161c61acb408b31e37953ea30
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2024-06-21 16:26:50 -0400

    add JSDoc to setTheme

--- src/lib/setTheme.ts
+++ src/lib/setTheme.ts
@@ -1,3 +1,8 @@
+/**
+ * Set the theme for the application
+ * @param selectedThemeVal - The string value of the selected theme
+ * @example setTheme('rocket'); //where 'rocket' is a theme defined folowing the `addingthemes.md` guide
+ */
 export function setTheme(selectedThemeVal: string): void {
 	document.body.setAttribute('data-theme', selectedThemeVal);
 }
