cab8b09ce14e017516086fe1b0caa2e8c7b5f5af
Anthony M <38088724+Creeperman1524@users.noreply.github.com>
2024-09-30 02:06:45 -0400

    Added a function to rate plans
    Added a library function that takes a plan and returns the amount of minutes spent on campus

---
+++ src/lib/ratePlan.ts
@@ -0,0 +1,70 @@
+import type { IPlan } from './interfaces/Plans';
+
+// Converts a Date to time in minutes since midnight
+function convertToMinutes(time: Date) : number {
+	console.log(time, time.getHours(), time.getMinutes());
+	return time.getHours() * 60 + time.getMinutes();
+}
+
+// Converts the NJIT day scheme to an index (0 = Sunday)
+function convertDayToIndex(day: string) : number {
+	switch (day) {
+		case 'U':
+			return 0	;
+		case 'M':
+			return 1	;
+		case 'T':
+			return 2	;
+		case 'W':
+			return 3	;
+		case 'R':
+			return 4	;
+		case 'F':
+			return 5	;
+		case 'S':
+			return 6	;
+		default:
+			return -1;
+	}
+}
+
+// Returns the amount of minutes a plan would spend on campus
+// "On campus" being from the start of the first class to the end of the last class on a given day
+export function ratePlan(plan: IPlan) : number {
+	const earliestStart = [1440, 1440, 1440, 1440, 1440, 1440, 1440];
+	const latestEnd = [0, 0, 0, 0, 0, 0, 0];
+	
+	for(const course of plan.courses) {
+		let section;
+
+		// Gets the selected section for that course
+		for(const s of course.sections) {
+			if(s.SECTION == course.selectedSection) section = s;
+		}
+		if(!section) continue;
+
+		// Loops through each meeting of that day
+		for(const meeting of section.TIMES) {
+			// Gets the start and end time and sees if it earlier/later than our currently stored start/end time for that day
+			const start = convertToMinutes(new Date(meeting.start));	
+			const end = convertToMinutes(new Date(meeting.end));
+
+			const index = convertDayToIndex(meeting.day);			
+
+			earliestStart[index] = Math.min(earliestStart[index], start);
+			latestEnd[index] = Math.max(latestEnd[index], end);
+		}
+	}
+
+	// Adds together the "time on campus" for a metric on how "good" the class is
+	let timeOnCampus = 0;
+	
+	for (let i = 0; i < 7; i++) {
+		const diff = latestEnd[i] - earliestStart[i];
+		if(diff < 0) continue; // No classes on that day
+	
+		timeOnCampus += diff;
+	}
+	
+	return timeOnCampus;
+}
