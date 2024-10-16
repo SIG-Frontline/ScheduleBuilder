995a728094e58054ec1871c9ce5fa811c13561a2
Mikel Alcazar <mklalcazar@gmail.com>
2024-10-16 01:48:42 -0400

    Added api/availableCourses?taken=

--- src/routes/api/availableCourses/+server.ts
+++ src/routes/api/availableCourses/+server.ts
@@ -1,7 +1,8 @@
 import { courseCache, sectionsCollection, type CourseDocument, type ReqTree } from '$lib/mongoClient';
-import { check_prereq } from '$lib/apiUtils.js';
+import { check_prereq, check_tree} from '$lib/apiUtils.js';
 import type { RequestEvent } from './$types';
 import { get_static_data } from '$lib/apiUtils.js';
+import type { REPLCommand } from 'repl';
 
 //http://localhost:XXXX/api/availableCourses?taken=CS100
 export async function GET( {url} ) {
@@ -21,7 +22,10 @@ export async function GET( {url} ) {
             }
         });
         */
-        return new Response(takenClasses?.toString(), { status: 200 });
+        return new Response(JSON.stringify(availableClasses), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' }
+        });
     } 
     const noPrereqClasses = await getAvailableClasses(); 
     return new Response(JSON.stringify({ noPrereqClasses }), {
@@ -32,34 +36,112 @@ export async function GET( {url} ) {
     });
 }
 
+function invertTree(possible_prereqs: Map<string, null|string[]>){
+    const out = new Map<string, null|string[]>();
+
+    for(const [k,v] of possible_prereqs.entries()){
+        if(v == null) continue;
+        for( const i in v){
+            if (!out.has(v[i])) 
+                out.set(v[i],[]);
+            out.get(v[i])?.push(k);
+        }
+    }
+    return out;
+}
+
+async function flattenTree(tree: null|string|string[]|ReqTree) {
+    //console.log(tree);
+
+    if (tree == null) return null;
+
+    const courses: string[] = [];
+
+    if (typeof tree === 'string') {
+        courses.push(tree);
+        return courses;
+    }
+
+    for (const i in tree){
+        if (Array.isArray(tree[i])){
+            const flatTree = await flattenTree(tree[i]);
+            //console.log("FlatTree:", flatTree)
+            if(flatTree != null){
+                courses.push(...flatTree);
+                //console.log("Concat:",courses)
+            }
+
+        } else if(tree[i] != '|' && tree[i] !='&')
+            courses.push(tree[i]);
+        //console.log("BRANCH:",courses);
+    }
+
+    return courses
+
+}
+
 //Returns the classes that are available with the classes taken
-//If takenClassses is undefined then returns all classes with no prereqs
+//If takenClasses is undefined then returns all classes with no prereqs
 async function getAvailableClasses(takenClasses?: string[]) {
     
     const static_courses: CourseDocument[] = await courseCache.getCourses();
-
+    
     //Filter Rutgers courses 
     const rutgers_regex = /^R\d{3}/;
     const NJITCourses = static_courses.filter((course) => {
         return !rutgers_regex.test(course._id); 
     });
-
+    
     if(takenClasses == null){
         return NJITCourses.filter(course => course.tree === null);
-     }
-
+    }
+    
     const courseMap = new Map<string, CourseDocument>();
     NJITCourses.forEach(course => {
         courseMap.set(course._id, course);  
     });
 
-   //console.log(static_courses);
+    //console.log(static_courses);
     console.log("here");
-    console.log(courseMap.get('CS 100'));
-
+    console.log(courseMap.get('CS 350'));
     
-    //Getch course tree, invert the tree, filter by prereq
+    const possible_prereqs = new Map<string, null|string[]>();
+    for(const [k,v] of courseMap.entries()){
+        const branch = await flattenTree(v.tree);
+        possible_prereqs.set(k,branch);
+    }
+        
+    //invert the tree
+    const possible_available_classes = invertTree(possible_prereqs)
+    
+    //console.log(possible_available_classes);
+    
+    //filter courses
+    const availableClasses: string[] = [];
+    
+    const checked_courses = new Set<string>();
+    
+    for (const i in takenClasses){
+        const currCourse = possible_available_classes.get(takenClasses[i]);
+        for (const j in currCourse){
+            const courseName = currCourse[Number(j)];
+            const currCourseTree = courseMap.get(courseName)?.tree
+            if(currCourseTree == null)
+                continue;
 
+            if (courseName == "CS 350"){
 
-    return "oof";
+                console.log("AHHHHHH")
+                console.log(currCourseTree.length)
+                console.log(check_tree(currCourseTree,takenClasses))
+            }
+
+            if( !checked_courses.has(currCourse[Number(j)]) && check_tree(currCourseTree,takenClasses) )
+                availableClasses.push(courseName);
+            checked_courses.add(courseName)
+        }
+    }
+    
+    return availableClasses;
+    
 }
