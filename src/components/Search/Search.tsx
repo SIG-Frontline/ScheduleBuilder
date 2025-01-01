595a5f924569b42565a484d3987ffea9a55c1877
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2025-01-01 10:16:15 -0500

    the start of a very cool search bar

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,111 +1,86 @@
-import { useEffect, useState } from "react";
-import { Combobox, TextInput } from "@mantine/core";
-import { Chip, Group } from "@mantine/core";
+import { useEffect, useMemo, useState } from "react";
+import { Chip, TextInput, Group } from "@mantine/core";
 import { getSubjects } from "@/actions/getSubjects";
 import { getClasses } from "@/actions/getClasses";
 
 export default function Search() {
-  const [value, setValue] = useState<string>("");
+  const [textBoxValue, setTextBoxValue] = useState<string>("");
   const [subjectOptions, setSubjectOptions] = useState<string[]>([]);
   const [classOptions, setClassOptions] = useState<string[]>([]);
   const [selectedSubject, setSelectedSubject] = useState<string>("");
+  const searchWithoutSubject = useMemo(() => {
+    return textBoxValue.replace(selectedSubject, "").trim();
+  }, [textBoxValue, selectedSubject]);
+
   useEffect(() => {
     getSubjects(202490).then((res) => {
+      //server side fn to get subjects
       const courses = res.map((course: { SUBJECT: string }) => course.SUBJECT);
       setSubjectOptions(courses);
     });
-  }, []);
-  //remove the selected value from the list of options
-  const filteredSubjectOptions = !selectedSubject
-    ? subjectOptions.filter((option) =>
-        option.toLowerCase().includes(value.toLowerCase())
-      )
-    : [];
-  const filterSearhString = value.replace(selectedSubject, "").trim();
+  }, []); //on mount - no dependencies
+
+  const filteredSubjectOptions = subjectOptions.filter((option) =>
+    //filter the subject options based on the search value
+    option.includes(textBoxValue)
+  );
   const filteredClassOptions = classOptions.filter((option) =>
-    option.toLowerCase().includes(filterSearhString.toLowerCase())
+    //filter the class options based on the search value without the subject
+    option.includes(searchWithoutSubject)
   );
 
-  const handleChipClick = (event: React.MouseEvent<HTMLInputElement>) => {
-    if (event.currentTarget.value === value) {
-      setValue("");
-      if (subjectOptions.includes(value)) {
-        setSelectedSubject("");
-      }
-    }
-  };
+  const chipOptions = selectedSubject
+    ? filteredClassOptions
+    : filteredSubjectOptions;
   useEffect(() => {
-    //make the value uppercase
-    setValue(value.toUpperCase());
-  }, [value]);
+    //when the textbox value changes
+    setTextBoxValue(textBoxValue.toUpperCase()); //make the input uppercase
 
-  /*
-        getClasses(202490, value).then((res) => {
-        const nums = res.map(
+    if (subjectOptions.includes(textBoxValue)) {
+      //if there is a subject in the textbox like CS, and it is not set as the selected subject
+      setSelectedSubject(textBoxValue);
+      getClasses(202490, textBoxValue).then((res) => {
+        //server side fn to get classes for the subject - only called when a subject is selected
+        const classes = res.map(
           (course: { COURSE_NUMB: string }) => course.COURSE_NUMB
         );
-        console.log(nums);
-        
-        setClassOptions(nums);
+        setClassOptions(classes);
+        console.log("classes", classes);
       });
-  */
-  //do the above when the value is selected
-  useEffect(() => {
-    if (value !== "" && subjectOptions.includes(value)) {
-      setSelectedSubject(value);
-      getClasses(202490, value).then((res) => {
-        const nums = res.map(
-          (course: { COURSE_NUMB: string }) => course.COURSE_NUMB
-        );
-        console.log(nums);
-
-        setClassOptions(nums);
-      });
+    } else if (selectedSubject && textBoxValue.includes(selectedSubject)) {
+      //if the textbox value includes the already selected subject
+      //do nothing
+    } else {
+      setSelectedSubject(""); //reset the selected subject if the textbox value does not include it
     }
-  }, [value]);
+  }, [textBoxValue, subjectOptions, selectedSubject, searchWithoutSubject]);
 
   return (
-    <Combobox>
-      <Combobox.EventsTarget>
-        <TextInput
-          placeholder="Pick value"
-          value={value}
-          onChange={(event) => setValue(event.currentTarget.value)}
-        />
-      </Combobox.EventsTarget>
-      <Chip.Group multiple={false} value={value} onChange={setValue}>
-        <Group>
-          <Combobox.Options display={"flex"} className="flex-row">
-            {selectedSubject && (
-              <Combobox.Option key={selectedSubject} value={selectedSubject}>
-                <Chip
-                  className="flex items-center justify-center"
-                  onClick={handleChipClick}
-                  value={selectedSubject}
-                  checked={true}
-                >
-                  {selectedSubject}
-                </Chip>
-              </Combobox.Option>
-            )}
-
-            {[...filteredSubjectOptions, ...filteredClassOptions].map(
-              (option) => (
-                <Combobox.Option key={option} value={option}>
-                  <Chip
-                    className="flex items-center justify-center"
-                    onClick={handleChipClick}
-                    value={option}
-                    checked={filteredClassOptions.length === 1}
-                  >
-                    {option}
-                  </Chip>
-                </Combobox.Option>
-              )
-            )}
-          </Combobox.Options>
+    <>
+      <TextInput
+        placeholder="Pick value"
+        value={textBoxValue}
+        onChange={(event) => setTextBoxValue(event.currentTarget.value)}
+      />
+      <Chip.Group
+        multiple={false}
+        onChange={(val) => {
+          setTextBoxValue(textBoxValue + val);
+        }}
+      >
+        <Group className="flex flex-row !flex-nowrap py-2">
+          {selectedSubject}
+          {chipOptions.map((option) => (
+            <Chip
+              key={option}
+              className="flex items-center justify-center"
+              value={option}
+            >
+              {option}
+            </Chip>
+          ))}
         </Group>
       </Chip.Group>
-    </Combobox>
+    </>
   );
 }
