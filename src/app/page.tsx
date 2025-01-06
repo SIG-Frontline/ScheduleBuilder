9d740d5da793eb0f12c93eb1eea29564a9ce6853
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2025-01-06 08:36:23 -0500

    remove old code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import Cal_Grid from "@/components/Cal_Grid/Cal_Grid";
 export default function Home() {
   return (
     <>
-      {/* <span className="!overflow-auto relative"> */}
       <Cal_Grid />
-      {/* </span> */}
     </>
   );
 }
