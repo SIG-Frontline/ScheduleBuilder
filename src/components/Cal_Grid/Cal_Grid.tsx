ad3b7fd1109901d42d5f37772ed275e3be455aa6
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2025-01-07 15:56:44 -0500

    grid refactor with fullcalendar

--- src/components/Cal_Grid/Cal_Grid.tsx
+++ src/components/Cal_Grid/Cal_Grid.tsx
@@ -1,117 +1,70 @@
 "use client";
-
-import React from "react";
-import { Table } from "@mantine/core";
-import Cal_Event from "../Cal_Event/Cal_Event";
-import { useResizeObserver } from "@mantine/hooks";
-
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
 const Cal_Grid = () => {
-  const timestamp_start = 5;
-  const timestamp_end = 21;
-  const Days_Of_Week = [
-    {
-      day: "Sunday",
-      enabled: false,
-    },
-    {
-      day: "Monday",
-      enabled: true,
-    },
-    {
-      day: "Tuesday",
-      enabled: true,
-    },
-    {
-      day: "Wednesday",
-      enabled: true,
-    },
-    {
-      day: "Thursday",
-      enabled: true,
-    },
-    {
-      day: "Friday",
-      enabled: true,
-    },
-    {
-      day: "Saturday",
-      enabled: true,
-    },
-  ];
-
-  const timestamps = (() => {
-    const timestamps = [];
-    for (let i = timestamp_start; i <= timestamp_end; i++) {
-      timestamps.push(
-        new Date(0, 0, 0, i).toLocaleTimeString([], {
-          hour: "numeric",
-          minute: "2-digit",
-          hour12: true,
-        })
-      );
-    }
-    return timestamps;
-  })();
-  const [dayref, dayrect] = useResizeObserver();
-  const [timeref, timerect] = useResizeObserver();
-  const rows = timestamps.map((ts) => (
-    <Table.Tr key={ts}>
-      <Table.Td ref={timeref} className={"sticky left-0"} w={100}>
-        {ts}
-      </Table.Td>
-      {Days_Of_Week.map((day) => {
-        if (day.enabled) {
-          return <Table.Td ref={dayref} key={day.day}></Table.Td>;
-        }
-      })}
-    </Table.Tr>
-  ));
   return (
-    <div className="relative">
-      <Cal_Event
-        top={350}
-        left={timerect?.width + 4 * dayrect?.width}
-        width={dayrect?.width}
-        height={100}
-      />
-      <Cal_Event
-        top={350}
-        left={timerect?.width + 2 * dayrect?.width}
-        width={dayrect?.width}
-        height={100}
-      />
-      <Table
-        horizontalSpacing={0}
-        layout="fixed"
-        stickyHeader
-        stickyHeaderOffset={60}
-        withColumnBorders
-        withRowBorders={false}
-        style={{
-          marginBottom: "8rem",
-          pointerEvents: "none",
-          minWidth: "60em",
-        }}
-      >
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th className={"sticky left-0"} w={100}>
-              Time
-            </Table.Th>
-            {Days_Of_Week.map((day) => {
-              if (day.enabled) {
-                return (
-                  <Table.Th key={day.day} ta="center">
-                    {day.day}
-                  </Table.Th>
-                );
-              }
-            })}
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>{rows}</Table.Tbody>
-      </Table>
-    </div>
+    <FullCalendar
+      plugins={[timeGridPlugin]}
+      slotLabelClassNames={"transform -translate-y-1/2 bg-white"}
+      timeZone="America/New_York"
+      initialView="timeGridWeek"
+      headerToolbar={{
+        left: "",
+        center: "",
+        right: "",
+      }}
+      stickyHeaderDates={true}
+      dayHeaderFormat={{
+        weekday: "long",
+      }}
+      events={[
+        {
+          title: "Repeating Event",
+          startTime: "08:00:00+00:00",
+          daysOfWeek: [1, 2, 4],
+          endTime: "12:00:00+00:00",
+        },
+      ]}
+      allDaySlot={false}
+      nowIndicator={false}
+      eventContent={(eventContent) => (
+        <div>
+          <i>{eventContent.event.title}</i>
+          <br />
+          <b>{eventContent?.timeText}</b>
+        </div>
+      )}
+      businessHours={[
+        {
+          daysOfWeek: [0, 1, 3, 4, 6],
+          startTime: "06:00:00",
+          endTime: "22:00:00",
+        },
+        {
+          daysOfWeek: [2],
+          startTime: "06:00:00",
+          endTime: "14:30:00",
+        },
+        {
+          daysOfWeek: [2],
+          startTime: "18:00:00",
+          endTime: "22:00:00",
+        },
+        {
+          daysOfWeek: [5],
+          startTime: "06:00:00",
+          endTime: "11:30:00",
+        },
+        {
+          daysOfWeek: [5],
+          startTime: "13:00:00",
+          endTime: "22:00:00",
+        },
+      ]}
+      slotMinTime={"06:00:00"}
+      slotMaxTime={"22:00:00"}
+      // eventClassNames="!bg-green-500"
+    />
   );
 };
 
