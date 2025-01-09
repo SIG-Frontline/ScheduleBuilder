f94562ca9ec67648e361fef211b5ce9bff94550d
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2025-01-09 11:33:41 -0500

    online courses display

--- src/components/Cal_Grid/Cal_Grid.tsx
+++ src/components/Cal_Grid/Cal_Grid.tsx
@@ -3,7 +3,7 @@ import { dayStore } from "@/lib/dayStore";
 import { Plan, planStore } from "@/lib/planStore";
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import { useComputedColorScheme } from "@mantine/core";
+import { Group, useComputedColorScheme } from "@mantine/core";
 import { useEffect, useState } from "react";
 const Cal_Grid = () => {
   const computedColorScheme = useComputedColorScheme("light", {
@@ -33,7 +33,6 @@ const Cal_Grid = () => {
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
   let eventData = currentSelectedPlanObj?.courses?.map((item) => {
     const courseCode = item.code;
     const sections = item.sections;
@@ -58,6 +57,7 @@ const Cal_Grid = () => {
           daysOfWeek: [
             ["U", "M", "T", "W", "R", "F", "S"].indexOf(meetingTime.day),
           ],
+          backgroundColor: "#00aa00",
         };
       });
     });
@@ -69,61 +69,85 @@ const Cal_Grid = () => {
   ];
 
   return (
-    <FullCalendar
-      height={"100%"}
-      plugins={[timeGridPlugin]}
-      slotLabelClassNames={`transform -translate-y-1/2 ${
-        computedColorScheme === "dark" ? "bg-[#242424]" : "bg-white"
-      } data-[time="06:00:00"]:opacity-0`}
-      timeZone="America/New_York"
-      initialView="timeGridWeek"
-      headerToolbar={false}
-      stickyHeaderDates={true}
-      dayHeaderFormat={{
-        weekday: "long",
-      }}
-      hiddenDays={day_store.days}
-      events={eventData}
-      allDaySlot={false}
-      nowIndicator={false}
-      eventContent={(eventContent) => (
-        <div>
-          <i>{eventContent.event.title}</i>
-          <br />
-          <b>{eventContent?.timeText}</b>
-        </div>
-      )}
-      businessHours={[
-        {
-          daysOfWeek: [0, 1, 2, 4, 6],
-          startTime: "06:00:00",
-          endTime: "22:00:00",
-        },
-        {
-          daysOfWeek: [3],
-          startTime: "06:00:00",
-          endTime: "14:30:00",
-        },
-        {
-          daysOfWeek: [3],
-          startTime: "18:00:00",
-          endTime: "22:00:00",
-        },
-        {
-          daysOfWeek: [5],
-          startTime: "06:00:00",
-          endTime: "11:30:00",
-        },
-        {
-          daysOfWeek: [5],
-          startTime: "13:00:00",
-          endTime: "22:00:00",
-        },
-      ]}
-      slotMinTime={"06:00:00"}
-      slotMaxTime={"22:00:00"}
-      // eventClassNames="!bg-green-500"
-    />
+    <>
+      <FullCalendar
+        height={"90%"}
+        plugins={[timeGridPlugin]}
+        slotLabelClassNames={`transform -translate-y-1/2 ${
+          computedColorScheme === "dark" ? "bg-[#242424]" : "bg-white"
+        } data-[time="06:00:00"]:opacity-0`}
+        timeZone="America/New_York"
+        initialView="timeGridWeek"
+        headerToolbar={false}
+        stickyHeaderDates={true}
+        dayHeaderFormat={{
+          weekday: "long",
+        }}
+        hiddenDays={day_store.days}
+        events={eventData}
+        allDaySlot={false}
+        nowIndicator={false}
+        eventContent={(eventContent) => (
+          <div>
+            <i>{eventContent.event.title}</i>
+            <br />
+            <b>{eventContent?.timeText}</b>
+          </div>
+        )}
+        businessHours={[
+          {
+            daysOfWeek: [0, 1, 2, 4, 6],
+            startTime: "06:00:00",
+            endTime: "22:00:00",
+          },
+          {
+            daysOfWeek: [3],
+            startTime: "06:00:00",
+            endTime: "14:30:00",
+          },
+          {
+            daysOfWeek: [3],
+            startTime: "18:00:00",
+            endTime: "22:00:00",
+          },
+          {
+            daysOfWeek: [5],
+            startTime: "06:00:00",
+            endTime: "11:30:00",
+          },
+          {
+            daysOfWeek: [5],
+            startTime: "13:00:00",
+            endTime: "22:00:00",
+          },
+        ]}
+        slotMinTime={"06:00:00"}
+        slotMaxTime={"22:00:00"}
+        // eventClassNames="!bg-green-500"
+      />
+      <Group>
+        {/* online courses */}
+        {currentSelectedPlanObj?.courses?.map((item) => {
+          const courseCode = item.code;
+          const sections = item.sections;
+          const courseTitle = item.title;
+          const onlineSections = sections.filter(
+            (section) => section.selected && section.meetingTimes.length === 0
+          );
+          return onlineSections.map((section) => {
+            return (
+              <div key={section.crn} className="flex items-center space-x-2">
+                <div className="w-4 h-4 bg-green-500 rounded-full"></div>
+                <div>
+                  <div>{courseCode + " " + courseTitle}</div>
+                  <div>Online</div>
+                </div>
+              </div>
+            );
+          });
+        })}
+      </Group>
+    </>
   );
 };
 
