05d73677130384ed9f46749202b5f642f145cfa0
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2025-01-09 12:48:01 -0500

    clean up settings ui

--- src/components/Tabs/Tab_Settings/Tab_Settings.tsx
+++ src/components/Tabs/Tab_Settings/Tab_Settings.tsx
@@ -4,6 +4,7 @@ import {
   ActionIcon,
   Group,
   Switch,
+  Text,
   useMantineColorScheme,
 } from "@mantine/core";
 import React from "react";
@@ -24,7 +25,19 @@ const Tab_Settings = () => {
   const { toggleColorScheme } = useMantineColorScheme();
   return (
     <>
-      <Group mt="xs">
+      <Group m="sm">
+        <ActionIcon className="m-1" variant="light" onClick={toggleColorScheme}>
+          <Icon>
+            <p className="dark:hidden">light_mode</p>
+            <p className="hidden dark:block">dark_mode</p>
+          </Icon>
+        </ActionIcon>
+        <Text size="md">Color Scheme</Text>
+      </Group>
+      <Text size="md" ta={"center"}>
+        Days of the Week
+      </Text>
+      <Group m="xs" className="flex !items-start !flex-col">
         {days.map((day) => (
           <Switch
             key={day.value}
@@ -34,12 +47,6 @@ const Tab_Settings = () => {
           />
         ))}
       </Group>
-      <ActionIcon className="m-5" variant="light" onClick={toggleColorScheme}>
-        <Icon>
-          <p className="dark:hidden">light_mode</p>
-          <p className="hidden dark:block">dark_mode</p>
-        </Icon>
-      </ActionIcon>
     </>
   );
 };
