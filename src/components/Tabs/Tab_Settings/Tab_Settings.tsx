e79bb57b43f25b096119ea9afde977e948aacb07
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2025-01-10 10:01:24 -0500

    better day hidding ui

--- src/components/Tabs/Tab_Settings/Tab_Settings.tsx
+++ src/components/Tabs/Tab_Settings/Tab_Settings.tsx
@@ -3,22 +3,21 @@ import { dayStore } from "@/lib/dayStore";
 import {
   ActionIcon,
   Group,
-  Switch,
+  MultiSelect,
   Text,
   useMantineColorScheme,
 } from "@mantine/core";
 import React from "react";
 
 const Tab_Settings = () => {
-  // const days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
   const days = [
-    { label: "Su", value: 0 },
-    { label: "Mo", value: 1 },
-    { label: "Tu", value: 2 },
-    { label: "We", value: 3 },
-    { label: "Th", value: 4 },
-    { label: "Fr", value: 5 },
-    { label: "Sa", value: 6 },
+    { label: "Su", value: "0" },
+    { label: "Mo", value: "1" },
+    { label: "Tu", value: "2" },
+    { label: "We", value: "3" },
+    { label: "Th", value: "4" },
+    { label: "Fr", value: "5" },
+    { label: "Sa", value: "6" },
   ];
   const day_store = dayStore();
 
@@ -37,16 +36,17 @@ const Tab_Settings = () => {
       <Text size="md" ta={"center"}>
         Days of the Week
       </Text>
-      <Group m="xs" className="flex !items-start !flex-col">
-        {days.map((day) => (
-          <Switch
-            key={day.value}
-            label={day.label}
-            defaultChecked={!day_store.days.includes(day.value)}
-            onChange={() => day_store.toggleDay(day.value)}
-          />
-        ))}
-      </Group>
+      <MultiSelect
+        mx="md"
+        label="Hidden days"
+        placeholder="Select hidden days"
+        data={days}
+        maxValues={6}
+        onChange={(values) => {
+          day_store.setDays(values.map((day) => parseInt(day)));
+        }}
+        value={day_store.days.map((day) => day.toString())}
+      />
     </>
   );
 };
