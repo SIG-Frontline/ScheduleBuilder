21d5397be0405a66434fed0e9eb0903dc634e7c8
Alejandro-Gonzale-z <greeny090315@gmail.com>
2025-04-09 13:16:01 -0400

    edited backend offline message

--- src/components/BackendOffline/BackendOfflineMessage.tsx
+++ src/components/BackendOffline/BackendOfflineMessage.tsx
@@ -1,7 +1,13 @@
 const BackendOfflineMessage = () => {
   return (
     <div className="text-center p-10">
-      <h1 className="text-6xl font-bold text-red-500">Backend is offline</h1>
+      <h1 className="text-6xl font-bold text-red-500">
+        Service is unavailable.
+      </h1>
+      {/* Easter Egg Error Code, if you know you know */}
+      <p className="text-xl font-semibold mt-4">
+        Error Code: 1000-01-1010-101-0-10-100 
+      </p>
       <p className="text-xl font-semibold mt-4">Please try again later.</p>
     </div>
   );
