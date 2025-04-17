a197340ca0e227f336ea860a7a7e9f89ec2387d7
WokFriedE <47959663+WokFriedE@users.noreply.github.com>
2025-04-17 19:50:59 -0400

    Useeffect fix

--- src/components/Nav/Tabs/Tab_Plans/Plans/NewPlanButton.tsx
+++ src/components/Nav/Tabs/Tab_Plans/Plans/NewPlanButton.tsx
@@ -43,6 +43,7 @@ const NewPlanButton = () => {
       "Plan Term": (value) => (value ? null : "Please select a term"),
     },
   });
+  useEffect(()=>{
   if (terms.length === 0) {
     setTimeout(() => {
       //set timeout of 0 to delay the loading of terms until the callstack is empty
@@ -65,6 +66,7 @@ const NewPlanButton = () => {
       });
     }, 0);
   }
+})
   const plan_store = planStore();
 
   function addPlan(form_values: {
