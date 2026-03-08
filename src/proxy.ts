8d53ef98b6d6554f548b758b714bb81390e777ac
Ethan H <47959663+WokFriedE@users.noreply.github.com>
2026-03-07 21:58:51 -0500

    Chore/next16 (#209)
    * package updates
    
    * change from middleware to proxy
    
    * packages
    
    * disable lint for now

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import { auth0 } from './lib/auth0';
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const url = request.nextUrl;
 
   // // Redirect from callback if Auth0 denied access
